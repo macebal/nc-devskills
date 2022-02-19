e8e3b4f743e781f102471e18f1dfa7ab6e7c50ae
Mariano Acebal <macebal@outlook.com.ar>
2022-02-19 19:50:03 -0300

    Updated Footer

--- website/src/components/Footer.js
+++ website/src/components/Footer.js
@@ -7,7 +7,7 @@ const Footer = () => {
             <a href="/" className="mb-3 me-2 mb-md-0 text-muted text-decoration-none lh-1">
                 <svg className="bi" width="30" height="24"></svg> {/*<use xlink:href="#bootstrap"/>*/}
             </a>
-            <span className="text-muted">&copy; 2021 Company, Inc</span>
+            <span className="text-muted">&copy; 2022 Mariano Acebal</span>
             </div>
         </footer>
 
